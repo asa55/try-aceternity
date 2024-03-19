ba87cc0e3f66c799c5c27c8fe873251758fd4c33
asa55 <asa55@pitt.edu>
2024-03-19 02:33:36 +0000

    add 3d card demo

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # try-aceternity-ui-components
 Trying Aceternity UI components. If you're reading this, buy their stuff.
+
+I'm learning from here: https://ui.aceternity.com/docs/install-nextjs
---
+++ my-app/src/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import { ThreeDCardDemo } from "./ThreeDCardDemo";
+
+export default function Home() {
+  return (
+    <>
+        <ThreeDCardDemo />
+    </>
+  );
+}
